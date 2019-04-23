472de7db1f7fca977b74e6b7c32638bc7b412805
duke mai <duc.mai@niteco.se>
2019-04-24 00:07:42 +0200

    menu

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,77 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
 import Link from 'next/link';
+import classnames from 'classnames';
 
 const linkStyle = {
   marginRight: 15,
 };
 
-const Header = ({ onSignUpClicked, onLogoClicked }) => (
-  <section className="title-bar">
-    <div className="title-bar-left">
-      <button className="menu-icon" type="button" />
-      <span onClick={onLogoClicked} className="title-bar-title">Businessy</span>
-    </div>
-    <div className="top-bar-right">
-      <ul className="menu">
-        <li>
-          <button onClick={onSignUpClicked} className="button small hollow" type="button">
-            Sign up
-          </button>
-        </li>
-      </ul>
-    </div>
-  </section>
-);
+const menuItems = [
+  { title: 'Home', link: '/' },
+  { title: 'Sign in', link: '/signin' },
+  { title: 'Profile', link: '/profile' },
+];
+
+const Header = ({ onSignUpClicked, onLogoClicked }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef();
+  const toggleMenu = e => {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    setIsOpen(!isOpen);
+  };
+  const documentClicked = e => {
+    if (!menuRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('click', documentClicked);
+    return () => {
+      document.removeEventListener('click', documentClicked);
+    };
+  });
+  return (
+    <>
+      <section className="title-bar">
+        <div className="title-bar-left">
+          <button onClick={toggleMenu} className="menu-icon" type="button" />
+          <span onClick={onLogoClicked} className="title-bar-title">
+            Businessy
+          </span>
+        </div>
+
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li>
+              <button
+                onClick={onSignUpClicked}
+                className="button small hollow"
+                type="button"
+              >
+                Sign up
+              </button>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div
+        ref={menuRef}
+        className={classnames('off-canvas position-left', {
+          'is-open': isOpen,
+        })}
+      >
+        <ul className="vertical menu mobile-ofc">
+          <li className="menu-text">Businessy</li>
+          {menuItems.map(menuItem => (
+            <li key={menuItem.link}>
+              <Link href={menuItem.link}>{menuItem.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Header;

a061efcc7383e91adc9bd52cefa5f63759311255
duke mai <duc.mai@niteco.se>
2019-04-24 23:22:26 +0200

    serverless config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
-const withStylus = require('@zeit/next-stylus')
-module.exports = withStylus()
+const withStylus = require('@zeit/next-stylus');
+module.exports = withStylus({
+  target: 'serverless',
+});

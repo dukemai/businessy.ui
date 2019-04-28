efa93cd288628a3ee7d61e82ed4c22996e7ce3be
duke mai <duc.mai@niteco.se>
2019-04-28 23:14:13 +0200

    add padding

--- pages/signup/confirm.js
+++ pages/signup/confirm.js
@@ -2,14 +2,17 @@ import Layout from '../../components/Layout';
 
 const Confirm = ({}) => (
   <Layout>
-    <section className="text--confirm">
-      <p>
-        We have emailed you a unique activation link to your work email address.
-      </p>
-      <p>
-        Click on the activation link and then we will start working on finding
-        the right CRM software for your business.
-      </p>
+    <section className="text--confirm grid-x grid-padding-x">
+      <div className="cell">
+        <p>
+          We have emailed you a unique activation link to your work email
+          address.
+        </p>
+        <p>
+          Click on the activation link and then we will start working on finding
+          the right CRM software for your business.
+        </p>
+      </div>
     </section>
   </Layout>
 );

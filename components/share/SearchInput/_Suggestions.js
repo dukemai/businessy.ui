edf424c42127ef246eaa6bcba71d6737ca11df22
duke mai <duc.mai@niteco.se>
2019-07-28 23:51:22 +0200

    fix suggestions

--- components/share/SearchInput/_Suggestions.js
+++ components/share/SearchInput/_Suggestions.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
+import SearchContext from '../../../SearchContext';
+
 const propTypes = {};
 const defaultProps = {};
-const Suggestions = ({ categories }) => (
-  <ul className="callout callout--suggestions cell--bordered">
-    {categories.map(cat => (
-      <li key={cat}>
-        <Link href={`/search?q=${cat}`}>
-          <a className="suggestion-link">{cat}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+const Suggestions = ({ categories }) => {
+  const { setSearchCategories, setSearchQuery } = useContext(SearchContext);
+  const onClicked = cat => e => {
+    setSearchCategories([]);
+    setSearchQuery(cat);
+  };
+  return (
+    <ul className="callout callout--suggestions cell--bordered">
+      {categories.map(cat => (
+        <li key={cat}>
+          <Link href={`/search?q=${cat}`}>
+            <a onClick={onClicked(cat)} className="suggestion-link">
+              {cat}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 Suggestions.propTypes = propTypes;
 Suggestions.defaultProps = defaultProps;
 export default Suggestions;
--- components/share/SearchInput/hoc.js
+++ components/share/SearchInput/hoc.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
-import { debounce, getOr } from 'lodash/fp';
+import { debounce, getOr, stubTrue, cond } from 'lodash/fp';
 
 import SearchContext from '../../../SearchContext';
 import { apiGET } from '../../../api';
@@ -19,34 +19,35 @@ const withAutoSuggest = Component => () => {
   const [isLoading, setIsloading] = useState(false);
   const [searchCategories, setSearchCategories] = useState([]);
 
-  const onQueryChange = ({ target }) => setSearchQuery(target.value);
+  const doSearch = debounce(200, async query => {
+    try {
+      console.log('...', query);
+      setIsloading(true);
+      const { data } = await apiGET('/search/software')({
+        params: { query },
+      });
+      setSearchCategories(data);
+    } catch (error) {
+    } finally {
+      setIsloading(false);
+    }
+  });
+  const getCategories = cond([
+    [q => q.length >= 3, doSearch],
+    [stubTrue, () => setSearchCategories([])],
+  ]);
+  const onQueryChange = ({ target }) => {
+    setSearchQuery(target.value);
+    if (target.value) {
+      getCategories(target.value);
+    }
+  };
   const onGetStarted = e => {
     Router.push(`/search?q=${encodeURIComponent(searchQuery)}`);
   };
+
   useEffect(() => {
     // /search/software?query=something
-    const doSearch = debounce(200, async () => {
-      try {
-        setIsloading(true);
-        const { data } = await apiGET('/search/software')({
-          params: { query: searchQuery },
-        });
-        setSearchCategories(data);
-      } catch (error) {
-      } finally {
-        setIsloading(false);
-      }
-    });
-    const getCategories = async () => {
-      if (searchQuery.length >= 3) {
-        doSearch();
-      } else {
-        setSearchCategories([]);
-      }
-    };
-    if (!query || searchQuery !== query) {
-      getCategories();
-    }
   }, [searchQuery]);
   return (
     <SearchContext.Provider
